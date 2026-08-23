5c6ac16825ba4af73eb6968871343dfb5ecfd0cb
CodeArc.studio <contact@codearc.studio>
2026-08-23 01:57:23 -0400

    Initial Commit

---
+++ dunno/dunnoApp.swift
@@ -0,0 +1,32 @@
+//
+//  dunnoApp.swift
+//  dunno
+//
+//  Created by Makai O'Neill on 8/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct dunnoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
